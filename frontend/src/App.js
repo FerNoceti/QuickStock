b70b6bd07db3bed1692c2af0649369317e8b33b5
Fernando José Noceti <fer.j.noceti@gmail.com>
2024-10-02 16:35:33 -0300

    NavBar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
 import "./styles/App.css";
 
 function App() {
   return (
     <div>
-      <ProductPage />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/products" element={<ProductPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
