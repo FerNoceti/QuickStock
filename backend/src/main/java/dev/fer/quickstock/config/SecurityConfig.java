7b6d6b2737f4f7c864f0456175b1e2b19ee407b9
Fernando José Noceti <fer.j.noceti@gmail.com>
2024-10-09 17:44:17 -0300

    Update SecurityConfig.java

--- backend/src/main/java/dev/fer/quickstock/config/SecurityConfig.java
+++ backend/src/main/java/dev/fer/quickstock/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package dev.fer.quickstock.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -34,5 +35,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
