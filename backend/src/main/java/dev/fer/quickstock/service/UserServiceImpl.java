e1e241350abf7ab999b100ece4ed7c2bb2d604cb
Fernando José Noceti <fer.j.noceti@gmail.com>
2024-10-11 01:37:19 -0300

    Update UserServiceImpl.java

--- backend/src/main/java/dev/fer/quickstock/service/UserServiceImpl.java
+++ backend/src/main/java/dev/fer/quickstock/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import dev.fer.quickstock.dto.User;
 import dev.fer.quickstock.dto.UserLogin;
 import dev.fer.quickstock.dto.UserResponse;
 import dev.fer.quickstock.repository.UserRepository;
+import dev.fer.quickstock.security.JwtTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,13 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenService jwtTokenService;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JwtTokenService jwtTokenService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtTokenService = jwtTokenService;
     }
 
     @Override
@@ -33,14 +36,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public ResponseEntity<String> loginUser(UserLogin userLogin) {
         User user = userRepository.findById(userLogin.getUsername()).orElse(null);
+
         if (user == null) {
             return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
-        if (bCryptPasswordEncoder.matches(userLogin.getPassword(), user.getPassword())) {
-            return new ResponseEntity<>("Login successful", HttpStatus.OK);
-        } else {
+
+        if (!bCryptPasswordEncoder.matches(userLogin.getPassword(), user.getPassword())) {
             return new ResponseEntity<>("Incorrect password", HttpStatus.UNAUTHORIZED);
         }
+
+        String token = jwtTokenService.generateToken(user.getUsername());
+
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
+
 
     }
