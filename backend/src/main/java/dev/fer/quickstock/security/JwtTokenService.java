6e77bca04c23a79d4517e2af541bf43980db0fdd
Fernando José Noceti <fer.j.noceti@gmail.com>
2024-10-11 01:12:01 -0300

    JwTokenService add

---
+++ backend/src/main/java/dev/fer/quickstock/security/JwtTokenService.java
@@ -0,0 +1,49 @@
+package dev.fer.quickstock.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtTokenService {
+
+    private final String SECRET_KEY = "secret";
+    private final long EXPIRATION_TIME = 864_000_000;
+
+    public String getSecretKey() {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject("username")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        String extractedUsername = extractUsername(token);
+        return !isTokenExpired(token) && extractedUsername.equals("username");
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
