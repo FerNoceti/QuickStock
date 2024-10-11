d5542541352ce22e67030b478f8013521804e92e
Fernando José Noceti <fer.j.noceti@gmail.com>
2024-10-11 01:37:15 -0300

    Update JwtTokenService.java

--- backend/src/main/java/dev/fer/quickstock/security/JwtTokenService.java
+++ backend/src/main/java/dev/fer/quickstock/security/JwtTokenService.java
@@ -2,6 +2,7 @@ package dev.fer.quickstock.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -11,20 +12,23 @@ import java.util.Map;
 @Service
 public class JwtTokenService {
 
-    private final String SECRET_KEY = "secret";
-    private final long EXPIRATION_TIME = 864_000_000;
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
-    public String getSecretKey() {
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME;
+
+    public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject("username")
+                .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
-
+    
     public String extractUsername(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET_KEY)
